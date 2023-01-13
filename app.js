020a8b4a866fcb1c81737a485fe58c3ce6493009
hvkst <bernd@hvkst.de>
2023-01-13 12:53:32 +0100

    fixing

--- app.js
+++ app.js
@@ -46,16 +46,16 @@ app.use('/contact', contactRoutes);
 const indexRoutes = require('./routes/index.routes');
 app.get('/', indexRoutes);
 
-app.all('*', (req, res) => {
-  res.status(404);
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 Not Found' });
-  } else {
-    res.type('txt').send('404 Not Found');
-  }
-});
+// app.all('*', (req, res) => {
+//   res.status(404);
+//   if (req.accepts('html')) {
+//     res.sendFile(path.join(__dirname, 'views', '404.html'));
+//   } else if (req.accepts('json')) {
+//     res.json({ message: '404 Not Found' });
+//   } else {
+//     res.type('txt').send('404 Not Found');
+//   }
+// });
 
 require('./error-handling')(app);
 
