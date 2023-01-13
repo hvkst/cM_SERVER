a8cfad9ff086caed9b4deb43f0c19a8057a93893
hvkst <bernd@hvkst.de>
2023-01-13 13:59:51 +0100

    cors fixed, for now...

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
-  // res.json('All good in here');
-});
-
-module.exports = router;
-
-// const path = require('path');
-
-// router.get('^/$|/index(.html)?', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+// router.get('/', (req, res, next) => {
+//   // res.json('All good in here');
 // });
 
 // module.exports = router;
+
+const path = require('path');
+
+router.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
+
+module.exports = router;
