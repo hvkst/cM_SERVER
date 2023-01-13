529abd4562a32959d3a4fb7d0942b35315da85c2
hvkst <bernd@hvkst.de>
2023-01-13 09:34:30 +0100

    create server index.html

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ app.use('/upload', uploadRoutes);
 const contactRoutes = require('./routes/contact.routes');
 app.use('/contact', contactRoutes);
 
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
+});
+
 require('./error-handling')(app);
 
 module.exports = app;
