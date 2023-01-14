fbb3622de8fbcfd2dc95e6088ec8eb14c3207df7
hvkst <bernd@hvkst.de>
2023-01-14 19:42:21 +0100

    last cors turns out to be an error handling issue

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -36,54 +36,46 @@ const router = express.Router();
 //   }
 // });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isLoggedOut, async (req, res) => {
   //  isLoggedOut, ?
   try {
     const userFromDb = await User.findOne({
       username: req.body.username,
     });
+    if (!userFromDb) throw new Error('There was an error during login. Please make sure username and password are correct.');
     const passwordsMatch = await validPassword(req.body.password, userFromDb.password);
-
     if (!passwordsMatch) {
-      console.log("Passwords didn't match");
-      throw new Error('Login failed');
+      throw new Error('There was an error during login. Please make sure username and password are correct.');
     }
 
-    console.log('Everything fine... on login');
-
+    // If user exists and password match create session
     const user = { id: userFromDb._id, username: userFromDb.username, isAdmin: userFromDb.admin };
     req.session.currentUser = user;
-    console.log(req.session.currentUser);
+    // console.log(req.session.currentUser);
 
-    // serialization => putting something like an object => into a value we can send. utf8 string.
     return res.json({
       message: 'Successfully logged in!',
       user,
     });
   } catch (error) {
-    console.log('There was an error', error);
-    return res.status(500).json({ error: 'There was an error during login. Please make sure username and password are correct.' });
-    // return res.status(500).json({ error: 'There was an error during login: ' + error.message });
+    return res.status(500).json({ error: 'There was an error during login: ' + error.message });
   }
 });
 
 router.post('/logout', (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
-    console.log('LOGGED OUT');
     res.json({ message: 'Successfully logged out!' });
   });
 });
 
 router.get('/verify', (req, res, next) => {
   try {
-    console.log(req.session);
     if (!req.session.currentUser) {
       return res.json({ message: 'No user here' });
     }
     return res.json({ user: req.session.currentUser });
   } catch (error) {
-    console.log(error);
     return res.status(403).json({ error: 'There was an error' });
   }
 });
